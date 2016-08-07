bfd2b111c37151e2dd5662fb3dc0ea1a7be2020c
ykevin <15018787835@163.com>
2016-08-07 16:38:20 +0800

    add amcl navigtion patch

--- buildRaspiRos/buildRaspiRos.sh
+++ buildRaspiRos/buildRaspiRos.sh
@@ -12,7 +12,7 @@ wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo 
 sudo sh -c 'echo "deb-src http://mirrordirector.raspbian.org/raspbian/ testing main contrib non-free rpi" >> /etc/apt/sources.list'
 
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
 echo "下载所需要的依赖包"
 sudo apt-get -y install git vim python-pip python-setuptools python-yaml python-distribute python-docutils python-dateutil python-six python-empy checkinstall cmake libboost-all-dev libtinyxml-dev libxml2-dev build-essential
@@ -36,56 +36,31 @@ mkdir ~/ros_catkin_ws
 cd ~/ros_catkin_ws
 
 echo "获取相关包的源代码到工作目录"
-#rosinstall_generator desktop --rosdistro indigo --deps --wet-only --exclude roslisp --tar > indigo-desktop-wet.rosinstall
-rosinstall_generator desktop --rosdistro indigo --deps --wet-only --exclude roslisp collada_parser collada_urdf --tar > indigo-desktop-wet.rosinstall
+rosinstall_generator desktop navigation move_base amcl map-server  nav-core --rosdistro indigo --deps --wet-only --exclude roslisp --tar > indigo-desktop-wet.rosinstall
 wstool init  src indigo-desktop-wet.rosinstall
 
 echo "用rosdep解决其它依赖问题"
 cd  ~/ros_catkin_ws
 rosdep install --from-paths src --ignore-src --rosdistro indigo -y -r --os=debian:jessie
 
-#echo "解决ROS GUI版本所需要的依赖问题"
-#mkdir ~/ros_catkin_ws/external_src
-
-#cd ~/ros_catkin_ws/external_src
-#sudo apt-get build-dep console-bridge
-#apt-get source -b console-bridge
-#sudo dpkg -i libconsole-bridge0.2*.deb libconsole-bridge-dev_*.deb
-
-#cd ~/ros_catkin_ws/external_src
-#apt-get source -b lz4
-#sudo dpkg -i liblz4-*.deb
-
-#echo "编译提示按enter键，到有选项提示时，选择2， 将包名改为liburdfdom-headers-dev, 接下来的两个问题连续选择'n'， 否则会编译出错"
-#cd ~/ros_catkin_ws/external_src
-#git clone https://github.com/ros/urdfdom_headers.git
-#cd urdfdom_headers
-#cmake .
-#sudo checkinstall make install
-
-#echo "编译提示按enter键，到有选项提示时，选择2， 将包名改为liburdfdom-dev, 接下来的两个问题连续选择'n'， 否则会编译出错"
-#cd ~/ros_catkin_ws/external_src
-#git clone https://github.com/ros/urdfdom.git
-#cd urdfdom
-#cmake .
-#sudo checkinstall make install 
-
-#echo "编译提示按enter键，到有选项提示时，选择2， 将包名改为collada-dom-dev, 接下来的两个问题连续选择'n'， 否则会编译出错"
-#cd ~/ros_catkin_ws/external_src
-#wget http://downloads.sourceforge.net/project/collada-dom/Collada%20DOM/Collada%20DOM%202.4/collada-dom-2.4.0.tgz
-#tar -xzf collada-dom-2.4.0.tgz
-#cd collada-dom-2.4.0
-#cmake .
-#sudo checkinstall make install
-
 echo "打上编译错误补丁"
-patch_dir="/home/pi/ros_catkin_ws/src/rviz/src/rviz/"
-patch_file="~/ros_catkin_ws/src/rviz/src/rviz/mesh_loader.cpp"
-if [ ! -f "$patch_file" ] ; then
-    sudo cp $DIR/rviz_mesh_loader.patch $patch_dir
-    cd $patch_dir
+rviz_dir="/home/pi/ros_catkin_ws/src/rviz/src/rviz/"
+rviz_file="~/ros_catkin_ws/src/rviz/src/rviz/mesh_loader.cpp"
+if [ ! -f "$rviz_file" ] ; then
+    sudo cp $DIR/rviz_mesh_loader.patch $rviz_dir
+    cd $rviz_dir
     patch -p0 < rviz_mesh_loader.patch  
 fi
+
+urdf_dir="/home/pi/ros_catkin_ws/src/robot_model/collada_urdf/src"
+urdf_file="~/ros_catkin_ws/src/robot_model/collada_urdf/src/collada_urdf.cpp"
+if [ ! -f "$urdf_file" ] ; then
+    sudo cp $DIR/collada_urdf.patch $urdf_dir
+    cd $urdf_dir
+    patch -p0 < collada_urdf.patch  
+fi
+
+
 
 echo "编译相关ROS 包"
 cd  ~/ros_catkin_ws
